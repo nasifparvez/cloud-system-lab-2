32fa84237fa580ad567927ca7472d7e71b8b2d0b
Nasif Parvez <34200192+nasifparvez@users.noreply.github.com>
2024-03-14 14:56:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,14 +28,13 @@ resource "aws_internet_gateway" "lab2-igw" {
 resource "aws_subnet" "public_sn" {
   count                   = 4
   vpc_id                  = aws_vpc.labtwo_vpc.id
-  cidr_block              = "192.168.${4 * count.index}.0/24"
+  cidr_block              = "192.168.${count.index}.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 
 resource "aws_route_table" "RT-public" {
-  count  = 4
   vpc_id = aws_vpc.labtwo_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -47,9 +46,11 @@ resource "aws_route_table" "RT-public" {
 resource "aws_route_table_association" "RT-public-sn" {
   count          = 4
   subnet_id      = aws_subnet.public_sn[count.index].id
-  route_table_id = aws_route_table.RT-public[count.index].id
+  route_table_id = aws_route_table.RT-public.id
 }
 
 output "vpc_id" {
   value = aws_vpc.labtwo_vpc.id
 }
+
+
